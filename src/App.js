ab0718d8a66ae0db71acbe05fef6ea8379631b16
Vishal Shaji <147316305+VishShaji@users.noreply.github.com>
2024-11-27 00:59:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
   };
 
   const handleLogin = () => {
-    window.location.href = 'https://ap-south-1squnbxjqf.auth.ap-south-1.amazoncognito.com/login?client_id=3h790bho4eq9je3ofgeuih854b&response_type=code&scope=email+openid+phone&redirect_uri=https://staging.d3bsnga2fhi5b6.amplifyapp.com/callback';
+    window.location.href = 'https://ap-south-1squnbxjqf.auth.ap-south-1.amazoncognito.com/login?client_id=3h790bho4eq9je3ofgeuih854b&response_type=code&scope=email+openid+phone&redirect_uri=https://master.d22iypo0elndm1.amplifyapp.com/callback';
   };
 
   const handleLogout = async () => {
