82952d70f0b81cad203a9e10cab31d2fe8c1dd2b
Vishal Shaji <147316305+VishShaji@users.noreply.github.com>
2024-11-27 17:07:59 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -8,34 +8,30 @@ import awsconfig from './aws-exports';
 const isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
 
 Amplify.configure({
-  Auth: {
-    Cognito: {
-      userPoolId: 'ap-south-1_sQunBxjQf',
-      userPoolClientId: '3h790bho4eq9je3ofgeuih854b',
-      loginWith: {
-        oauth: {
-          domain: 'minecraft-server-hosting.auth.ap-south-1.amazoncognito.com',
-          scopes: ['openid', 'email', 'profile'],
-          responseType: 'code',
-          redirectSignIn: isLocalhost 
-            ? 'http://localhost:3000/callback'
-            : 'https://YOUR-AMPLIFY-APP-URL/callback',
-          redirectSignOut: isLocalhost
-            ? 'http://localhost:3000'
-            : 'https://YOUR-AMPLIFY-APP-URL'
-        }
-      }
-    }
-  },
-  API: {
-    REST: {
-      api: {
-        endpoint: 'https://d6wrnrfjri.execute-api.ap-south-1.amazonaws.com/dev',
-        region: 'ap-south-1'
-      }
-    }
-  }
-});
+          Auth: {
+            Cognito: {
+              userPoolId: 'ap-south-1_sQunBxjQf',
+              userPoolClientId: '3h790bho4eq9je3ofgeuih854b',
+              loginWith: {
+                oauth: {
+                  domain: 'ap-south-1squnbxjqf.auth.ap-south-1.amazoncognito.com',
+                  scopes: ['email', 'openid', 'phone'],
+                  responseType: 'code',
+                  redirectSignIn: 'https://master.d22iypo0elndm1.amplifyapp.com/callback',
+                  redirectSignOut: 'https://master.d22iypo0elndm1.amplifyapp.com'
+                }
+              }
+            }
+          },
+          API: {
+            REST: {
+              api: {
+                endpoint: 'https://d6wrnrfjri.execute-api.ap-south-1.amazonaws.com/dev',
+                region: 'ap-south-1'
+              }
+            }
+          }
+        });
 
 const root = createRoot(document.getElementById('root'));
 root.render(
